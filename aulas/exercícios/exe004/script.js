7ed75857f8bfd9a3311ef47827115cc410c4053a
FCarneiroOliveira <107850122+FCarneiroOliveira@users.noreply.github.com>
2023-07-03 09:47:34 -0400

    exe 004

---
+++ aulas/exercícios/exe004/script.js
@@ -0,0 +1,17 @@
+function calcular(){
+    var n = document.getElementById('numero')
+    var tab = document.getElementById('tab')
+    if(n.value.length == 0){
+        window.alert('Digite um número!')
+    }else{
+        var n = Number(n.value)
+        tab.innerHTML = ''
+        for(c = 1; c <= 10; c++){
+            var r = document.createElement('option')
+            r.text = `${n}x${c} = ${n*c}`
+            r.value = `tab${c}`
+            tab.appendChild(r)
+        }   
+    }
+    
+}
